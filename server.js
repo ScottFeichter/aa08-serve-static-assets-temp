ca221e94d91729be41884568598f521873dfd8ed
Scott Feichter <scottfeichter@gmail.com>
2024-04-09 14:39:36 -0700

    static assets are in progress

--- server.js
+++ server.js
@@ -2,8 +2,42 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Your code here 
+  // Your code here
+
+  // static assets -------------------------------
+  if(req.method === `GET` && req.url === `/`) {
+    const resBody = fs.readFileSync('./index.html', `utf-8`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(resBody);
+  }
+
+  if(req.method === `GET` && req.url === `/static/css/application.css`) {
+    const resBody = fs.readFileSync('./static/assets/css/application.css', `text/css`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/css');
+    return res.end(resBody);
+  }
+
+  if(req.method === `GET` && req.url === `/static/assets/css/application.css`) {
+    const resBody = fs.readFileSync('./index.html');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(resBody);
+  }
+
+  if(req.method === `GET` && req.url === `/static/assets/images/dog.jpg`) {
+    const resBody = fs.readFileSync('./index.html');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'image/jpeg');
+    return res.end(resBody);
+  }
+
 });
+
+
+
+
 
 const port = 5000;
 
